254971d9f65976438ae79c93c4298b453a5e345d
Redcozmo <redcozmo@hotmail.com>
2018-11-05 21:16:48 +0100

    merge with Q TDD test

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_103958) do
+ActiveRecord::Schema.define(version: 2018_11_05_160226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "fname", null: false
-    t.string "lname", null: false
-    t.string "email", null: false
+    t.string "fname"
+    t.string "lname"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
